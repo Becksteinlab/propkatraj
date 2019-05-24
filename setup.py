b9bc66b2b14317935178f5758ea2096ccfd33d2a
Oliver Beckstein <orbeckst@gmail.com>
2019-05-24 11:22:18 -0700

    updated setup.py for 1.0.1
    - added meta data
    - release 1.0.1

--- setup.py
+++ setup.py
@@ -10,20 +10,33 @@ For a basic installation just type the command::
 from setuptools import setup, find_packages
 
 setup(name='propkatraj',
-      version='0.1.0',
+      version='1.0.1',
       description='obtain pKas for titreatable residues from a simulation trajectory',
       author='David Dotson',
       author_email='dotsdl@gmail.com',
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python',
-        'Topic :: Scientific/Engineering',
-        ],
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Environment :: Console',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+      ],
+      project_urls={
+          'Documentation': 'https://github.com/Becksteinlab/propkatraj/blob/master/README.md',
+          'Source': 'https://github.com/Becksteinlab/propkatraj',
+          'Issue Tracker': 'https://github.com/Becksteinlab/propkatraj/issues',
+      },
       packages=['propkatraj'],
       scripts=[],
       license='GPLv3',
       long_description=open('README.md').read(),
-      install_requires=['numpy', 'pandas', 'MDAnalysis', 'propka']
+      long_description_content_type='text/markdown; variant=GFM',
+      install_requires=['six', 'numpy', 'pandas', 'MDAnalysis', 'propka']
       )
