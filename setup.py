01b757f42f768f97fc49be1576ff193ca2ed3ffd
Ian Kenney <ikenney@asu.edu>
2023-01-31 14:01:06 -0700

    Removed find_packages import, added py3.8+ requirement, removed six install requirement

--- setup.py
+++ setup.py
@@ -7,14 +7,8 @@ For a basic installation just type the command::
 
 """
 
-import sys
-from setuptools import setup, find_packages
+from setuptools import setup
 import versioneer
-
-# Make sure we have the right python version (2.7+)
-if sys.version_info[:2] < (2, 7):
-    print('propkatraj requires python 2.7 or above')
-    sys.exit(-1)
 
 setup(name='propkatraj',
       version=versioneer.get_version(),
@@ -36,16 +30,17 @@ setup(name='propkatraj',
                    'Programming Language :: Python :: 3.6',
                    'Programming Language :: Python :: 3.7',
                    'Programming Language :: Python :: 3.8',
-      ],
+                   ],
       project_urls={
           'Documentation': 'https://github.com/Becksteinlab/propkatraj/blob/master/README.md',
           'Source': 'https://github.com/Becksteinlab/propkatraj',
           'Issue Tracker': 'https://github.com/Becksteinlab/propkatraj/issues',
-      },
+          },
       packages=['propkatraj'],
       scripts=[],
       license='GPLv3',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown; variant=GFM',
-      install_requires=['six', 'numpy', 'pandas', 'MDAnalysis', 'propka==3.1']
+      python_requires='>=3.8',
+      install_requires=['numpy', 'pandas', 'MDAnalysis>2.0.0', 'propka==3.1']
       )
