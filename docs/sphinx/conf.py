21df7590a5fc15564c46a5e6e8e7b9e29e820af0
Irfan Alibay <IAlibay@users.noreply.github.com>
2023-02-14 17:19:52 -0500

    fix sphinx rendering (#56)
    Fixes #55
    * add extra sphinx extensions to improve docs rendering

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -24,6 +24,13 @@ release = '1.1.0'
 extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'sphinx_sitemap',
+    'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
@@ -36,3 +43,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# Configuration for intersphinx: refer to the Python standard library
+# and other packages used by MDAnalysis
+intersphinx_mapping = {
+                       'https://docs.python.org/3/': None,
+                       'https://docs.mdanalysis.org/stable/index.html': None,
+                       'https://numpy.org/doc/stable/': None,
+                       'https://pandas.pydata.org/pandas-docs/stable/': None,
+                       }
